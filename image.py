402f5b8937d3c7c7172db7122c7c2176d920fc62
beckyw5 <becky_w5@hotmail.co.uk>
2020-06-14 18:04:52 +0100

    Save image which has been transformed

--- image.py
+++ image.py
@@ -4,6 +4,8 @@ from matplotlib import pyplot as plt
 
 
 img = cv2.imread('watch.jpg', cv2.IMREAD_GRAYSCALE)
+cv2.imwrite('watchgrey.png', img)
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
