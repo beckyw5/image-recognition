5f697a24e4dff0116ce5af27ee629eefccfd564c
beckyw5 <becky_w5@hotmail.co.uk>
2020-06-14 18:33:02 +0100

    If there is a feed return frame

--- video.py
+++ video.py
@@ -3,4 +3,8 @@ import cv2
 
 # 0 first webcam, 1 second webcam, 0 is standard webcam
 cap = cv2.VideoCapture(0)
+while True:
+    # if there is a feed return
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
 
